aaca75c89937e0afc7281f6271d1ac798fd85013
Omar Nashawati <oceanprime.on@gmail.com>
2026-05-06 19:25:55 +0300

    add style guidline create app main structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <header>
+        <h1>Color Generator</h1>
+        <form>
+          <input type="color" />
+          <input type="text" placeholder="#FFFFFF" />
+          <button className="btn">Generate</button>
+        </form>
+      </header>
+      <section>colors</section>
+    </main>
+  );
 };
 
 export default App;
